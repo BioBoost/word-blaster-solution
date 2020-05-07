b03cd4e28f545ee7c8cc4685c565120451021805
Nico De Witte <nico.dewitte@outlook.com>
2020-05-07 11:22:17 +0200

    Fix UserIO indentation

--- src/lib/user_io.cpp
+++ src/lib/user_io.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 namespace WordBlasterTheGame {
 
-      void UserIO::show_welcome(void) {
-        cout << "Welcome to Word Blaster" << endl;
-        cout << "Your next gen typing experience" << endl;
-      }
+  void UserIO::show_welcome(void) {
+    cout << "Welcome to Word Blaster" << endl;
+    cout << "Your next gen typing experience" << endl;
+  }
 
-      void UserIO::show_word_to_type(unsigned int number, std::string word) {
-        cout << "Word #" << (number+1) << endl;
-        cout << word << endl << endl;
-      }
+  void UserIO::show_word_to_type(unsigned int number, std::string word) {
+    cout << "Word #" << (number+1) << endl;
+    cout << word << endl << endl;
+  }
 
-      std::string UserIO::request_user_to_type_word(void) {
-        cout << "Type your word here [enter to pass]: ";
-        std::string userWord;
-        getline(cin, userWord);
-        return userWord;
-      }
+  std::string UserIO::request_user_to_type_word(void) {
+    cout << "Type your word here [enter to pass]: ";
+    std::string userWord;
+    getline(cin, userWord);
+    return userWord;
+  }
 
-      void UserIO::show_final_score(Score score) {
-        cout << endl << "Your final score is " << score.total_score() << endl;
-      }
+  void UserIO::show_final_score(Score score) {
+    cout << endl << "Your final score is " << score.total_score() << endl;
+  }
 
 };
